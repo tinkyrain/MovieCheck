da73edfb109fa6ba0ef8ef6f41baaf4d5fce4d63
vladislow <t1nky132@mail.ru>
2023-04-26 15:14:38 +0500

    add top line

--- src/components/InfoBlock.js
+++ src/components/InfoBlock.js
@@ -1,8 +1,23 @@
-import './InfoBlock.css'
+import './InfoBlock.css';
+import { useDispatch } from 'react-redux';
+
 
 const InfoBlock = () => {
+
+    const dispatch = useDispatch();
+
+    const back = () => {
+        dispatch({type: 'VISION', visionChange: true})
+    }
+
     return (
-        <p>Info Block</p>
+        <div className='infoBlock'>
+            <div className='topInfoLine'>
+                <button className='backBtn' onClick={() => back()}>
+                    <img className='backArrow' alt='Назад' src='/img/backArrow.png'/>
+                </button>
+            </div>
+        </div>
     );
 }
 
