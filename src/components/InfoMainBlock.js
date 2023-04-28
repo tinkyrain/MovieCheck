7f70512098e93d2a1e22e2a6a46a8fca7d9769c3
vladislow <t1nky132@mail.ru>
2023-04-28 15:51:38 +0500

    add layout info

--- src/components/InfoMainBlock.js
+++ src/components/InfoMainBlock.js
@@ -4,11 +4,18 @@ const InfoMainBlock = () => {
     return(
         <div className='InfoMainBlock'>
             <div className='InfoText'>
-                duuhd
+                <ul className='InfoList'>
+                    <li>Год: 2022</li>
+                    <li>Страны: США, КАНАДА, ЯПОНИЯ</li>
+                    <li>Жанры: Фантастика, Фэнтези, Боевик, Приключения, Комедия</li>
+                    <li>Актёры: Джеймс Марсден, Бен Шварц, Наташа Ротуэлл</li>
+                    <li>Мировые сборы: 1234567890$</li>
+                    <li>Премьера в мире: 25 января 2020 </li>
+                </ul>
             </div>
 
             <div className='InfoImg'>
-                влалвоажлва
+                <img src='/img/poster.jpg' alt='poster' className='poster'/>
             </div>
         </div>
     );
