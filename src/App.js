a893302610b9a15c28298b04230e17704fbd3a61
vladislow <t1nky132@mail.ru>
2023-05-01 11:07:11 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ function App() {
   return (
     <main>
       {
-        //vision ? <SearchBlock/> : <InfoBlock/>
+        vision ? <SearchBlock/> : <InfoBlock/>
       }
-      <SearchBlock/>
     </main>
   );
 }
