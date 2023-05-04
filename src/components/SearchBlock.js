27348302f426e2e22c3b76d08683340895d3b68d
vladislow <t1nky132@mail.ru>
2023-05-04 20:36:25 +0500

    API

--- src/components/SearchBlock.js
+++ src/components/SearchBlock.js
@@ -1,10 +1,20 @@
 import { useDispatch } from 'react-redux'
 import './SearchBlock.css'
 
+const KEY = '7f9a3b82';
+const URL = `http://www.omdbapi.com/?t=DARK&apikey=${KEY}`;
+
 const SearchBlock = () => {
     const dispatch = useDispatch();
 
+    const getData = () => {
+        fetch(`${URL}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }
+
     const search = () => {
+        getData();
         dispatch({type: 'VISION', visionChange: false})
     }
 
